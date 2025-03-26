55c8bc82b1894a64516c5253e5ab3ba4622387ad
Haridass S <137673520+Haridass9489@users.noreply.github.com>
2025-03-26 23:15:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'Generative AI Project',
     version= '0.0.0',
     author= 'Bappy Ahmed',
-    author_email= 'entbappy73@gmail.com',
+    author_email= 'hdhms5572@gmail.com',
     packages= find_packages(),
     install_requires = []
 
